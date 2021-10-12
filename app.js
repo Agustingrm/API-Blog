0849ec35c556ed490d8e6601bf959fecf910c365
Agustin Gramajo <agustingramajo@gmail.com>
2021-10-12 11:59:39 -0300

    Changed mondo-db-url

--- app.js
+++ app.js
@@ -72,7 +72,8 @@ app.use(function (req, res, next) {
 
 //Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = `${process.env.mongoDB}`;
+var dev_db_url = `${process.env.mongoDB}`;
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
