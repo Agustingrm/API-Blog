8cd987bb13974fd2684b78791e415f102bec8ca6
Agustin Gramajo <agustingramajo@gmail.com>
2021-10-13 20:40:08 -0300

    cors

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,7 +45,7 @@ exports.create_post = [
 ];
 
 exports.login_post = passport.authenticate("local", {
-  successRedirect: "/",
+  successRedirect: "/login",
   failureRedirect: "/login",
 });
 // exports.login_post = async (req, res, next) => {
