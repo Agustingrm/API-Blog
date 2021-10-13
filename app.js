6a209da31030b31eeb60623b337abaf9640ed96e
Agustin Gramajo <agustingramajo@gmail.com>
2021-10-13 10:52:41 -0300

    Cors

--- app.js
+++ app.js
@@ -26,6 +26,20 @@ app.use(helmet());
 app.use(cors());
 app.options('*', cors())
 
+/** HEADER START */
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,PUT');
+  next();
+});
+app.options("/*", function(req, res, next){
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With,x-access-token');
+  res.send(200);
+});
+/** HEADER END */
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -89,20 +103,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
-/** HEADER START */
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin','*');
-  res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,PUT');
-  next();
-});
-app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With,x-access-token');
-  res.send(200);
-});
-/** HEADER END */
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
