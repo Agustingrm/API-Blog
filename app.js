ec73180e8d7c4fce31ee03cdd6d52851e0a35ad9
Agustin Gramajo <agustingramajo@gmail.com>
2021-10-16 18:09:16 -0300

    Changed login method to jwt

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var passport = require("passport");
+// var passport = require("passport");
 const session = require("express-session");
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
@@ -49,40 +49,40 @@ app.set("view engine", "ejs");
 //Passport to persist sessions
 app.use(session({ secret: "rome", resave: false, saveUninitialized: true }));
 
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) {
-        return done(err);
-      }
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-      bcrypt.compare(password, user.password, (err, res) => {
-        if (res) {
-          // passwords match! log user in
-          return done(null, user);
-        } else {
-          // passwords do not match!
-          return done(null, false, { message: "Incorrect password" });
-        }
-      });
-    });
-  })
-);
+// passport.use(
+//   new LocalStrategy((username, password, done) => {
+//     User.findOne({ username: username }, (err, user) => {
+//       if (err) {
+//         return done(err);
+//       }
+//       if (!user) {
+//         return done(null, false, { message: "Incorrect username" });
+//       }
+//       bcrypt.compare(password, user.password, (err, res) => {
+//         if (res) {
+//           // passwords match! log user in
+//           return done(null, user);
+//         } else {
+//           // passwords do not match!
+//           return done(null, false, { message: "Incorrect password" });
+//         }
+//       });
+//     });
+//   })
+// );
 
-passport.serializeUser(function (user, done) {
-  done(null, user.id);
-});
+// passport.serializeUser(function (user, done) {
+//   done(null, user.id);
+// });
 
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
-    done(err, user);
-  });
-});
+// passport.deserializeUser(function (id, done) {
+//   User.findById(id, function (err, user) {
+//     done(err, user);
+//   });
+// });
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 //User available everywhere
 app.use(function (req, res, next) {
